45fcfc832e0e38df66970f73706e374c349d5b75
nigussiefolikia@gmail.com <your@email.com>
2025-07-20 20:52:59 +0300

    Fix: update views import and class-based view for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Book, Library
-from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
+from django.views.generic import DetailView
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
