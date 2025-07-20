4e1e00b25f300ea40dd0db8d77340b02c809ef7c
nigussiefolikia@gmail.com <your@email.com>
2025-07-20 20:26:41 +0300

    Fix query_samples.py with exact required ORM queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,18 +1,17 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author
-author_name = "Author Name"
-books_by_author = Book.objects.filter(author__name=author_name)
-print(books_by_author)
+# Query all books by a specific author
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print("Books by", author_name, ":", books_by_author)
 
-# 2. List all books in a specific library
-library_name = "Library Name"
+# List all books in a library
+library_name = "Central Library"
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print(books_in_library)
+print("Books in", library_name, ":", books_in_library)
 
-# 3. Retrieve the librarian for a library
-library_name = "Library Name"
-library = Library.objects.get(name=library_name)
+# Retrieve the librarian for a library
 librarian = Librarian.objects.get(library=library)
-print(librarian)
+print("Librarian of", library_name, ":", librarian)
