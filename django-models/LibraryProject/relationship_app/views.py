b5145c139c7a8203221b044c64324b4cea2305e7
nigussiefolikia@gmail.com <your@email.com>
2025-07-20 20:41:47 +0300

    Fix: Move templates into relationship_app directory and update paths

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,9 +4,9 @@ from django.views.generic.detail import DetailView
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 # Class-based view to show details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
